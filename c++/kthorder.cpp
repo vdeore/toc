5027f79e780bf8ac5543f3bb704be76172fbeca4
Vinay <vinbd007@gmail.com>
2015-05-27 22:56:18 -0700

    k-th order statistics

---
+++ c++/kthorder.cpp
@@ -0,0 +1,55 @@
+/* problem:
+ * Order Statistics. Kth Smallest or Largest Element in an Un-ordered Array.
+ */
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+void print(int a[], int n) {
+    cout <<"\n";
+    for (int i = 0; i < n; ++i) {
+        cout <<" "<<a[i];
+    }
+    cout <<"\n";
+}
+
+int partition(int *a, int l, int r) {
+    int m, p;
+    if (l == r) {
+        return l;
+    }
+    m = (l+r)/2;
+    p = a[m];
+    while (l <= r) {
+        while(l <= r && a[l] < p) ++l;
+        while(l <= r && a[r] > p) --r;
+        if (l <= r) {
+            swap(a[l], a[r]);
+            ++l; --r;
+        }
+    }
+    return l;
+}
+
+int kthorder(int *a, int l, int r, int k) {
+    int rank;
+
+    rank = partition(a, l, r);
+    if (rank == k-1) {
+        return a[k-1];
+    }
+    if (rank < k-1) {
+        return(kthorder(a, rank+1, r, k));
+    } 
+    return kthorder(a, l, rank-1, k);
+}
+
+main () {
+    int k = 6;
+    int a[] = {14,8,22,12,4,2,10};
+
+    cout<<"\n"<<k<<"th-order stats: "
+        <<kthorder(a, 0, sizeof(a)/sizeof(int)-1, k)
+        <<"\n";
+    sort(a, a+sizeof(a)/sizeof(int));
+}
